d2bab350beada9b7d14dee2562044d5e2e8c2caf
Zef Hemel <zef@zef.me>
2022-10-13 15:15:20 +0200

    Deno conversion

--- unfurl.ts
+++ unfurl.ts
@@ -1,4 +1,4 @@
-export async function unfurlOptions(url: string) {
+export function unfurlOptions(url: string) {
   if (/https:\/\/twitter.com\/[^\/]+\/status\/\d+/.exec(url)) {
     return [{ id: "tweet-unfurl", name: "Tweet content" }];
   } else {
@@ -16,15 +16,17 @@ function htmlToMarkdown(html: string) {
 }
 
 export async function unfurlTweet(url: string): Promise<string> {
-  let oEmbedResult = await fetch(
-    `https://publish.twitter.com/oembed?url=${url}`
+  const oEmbedResult = await fetch(
+    `https://publish.twitter.com/oembed?url=${url}`,
   );
   if (oEmbedResult.status !== 200) {
     console.error(await oEmbedResult.text());
     throw new Error("Failed to fetch oEmbed result");
   }
-  let oEmbedJson = await oEmbedResult.json();
-  return `[${oEmbedJson.author_name}](${url}):\n > ${htmlToMarkdown(
-    oEmbedJson.html
-  )}`;
+  const oEmbedJson = await oEmbedResult.json();
+  return `[${oEmbedJson.author_name}](${url}):\n > ${
+    htmlToMarkdown(
+      oEmbedJson.html,
+    )
+  }`;
 }
